98abaec0c9f619a3b9f325cfa876fd513bc82689
airtoxin <airtoxin@icloud.com>
2015-01-22 00:26:02 +0900

    Patch style

--- gulp/less.js
+++ gulp/less.js
@@ -3,6 +3,6 @@ var less = require( 'gulp-less' );
 
 gulp.task( 'less', function () {
 	return gulp.src( './style/**/*.less' )
-		pipe( less() )
+		.pipe( less() )
 		.pipe( gulp.dest( './compile/style' ) );
 } );
