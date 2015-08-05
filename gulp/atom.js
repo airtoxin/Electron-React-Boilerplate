ba68e98c2e224cac1ca8c4f34b9540c44e71e41d
airtoxin <airtoxin@icloud.com>
2015-08-06 00:56:07 +0900

    Update electron

--- gulp/atom.js
+++ gulp/atom.js
@@ -2,11 +2,11 @@ var gulp = require( 'gulp' );
 var atom = require( 'gulp-atom' );
 
 gulp.task( 'atom', function () {
-	return atom( {
+	atom( {
 		srcPath: './compile',
 		releasePath: './build',
 		cachePath: './cache',
-		version: 'v0.26.0',
+		version: 'v0.30.2',
 		rebuild: false,
 		platforms: [ 'darwin-x64' ]
 	} );
