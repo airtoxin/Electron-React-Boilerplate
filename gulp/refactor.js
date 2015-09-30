076d4f45ff4d053b361d3527469eaa9755945270
David Asabina <david@supr.nu>
2015-10-01 00:02:58 +0200

    specify glob for syntax-checkable files

--- gulp/refactor.js
+++ gulp/refactor.js
@@ -1,14 +1,15 @@
 var gulp = require( 'gulp' );
 var jscs = require( 'gulp-jscs' );
+var files_matcher = [ './component/**/*.js', './*.js' ];
 
-gulp.task( 'style', function() { 
-  return gulp.src([ './component/**/*.js', './index.js' ])
+gulp.task( 'check-syntax', function() { 
+  return gulp.src(files_matcher)
     .pipe(jscs())
     .pipe(jscs.reporter());
 } );
 
 gulp.task( 'refactor', function() {
-  return gulp.src([ './**/*.js' ])
+  return gulp.src(files_matcher)
     .pipe(jscs({
       fix: true
     }))
