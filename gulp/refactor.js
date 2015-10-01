6267a3cf3d85a94dbd7ce6eb7862553aabe5a6b2
David Asabina <david@supr.nu>
2015-10-01 17:52:33 +0200

    Set base on `check-syntax` task

--- gulp/refactor.js
+++ gulp/refactor.js
@@ -3,7 +3,7 @@ var jscs = require( 'gulp-jscs' );
 var files_matcher = [ 'component/**/*.js', 'gulp/**/*.js', '*.js' ];
 
 gulp.task( 'check-syntax', function() { 
-  return gulp.src(files_matcher)
+  return gulp.src(files_matcher, { base: './' })
     .pipe(jscs())
     .pipe(jscs.reporter());
 } );
