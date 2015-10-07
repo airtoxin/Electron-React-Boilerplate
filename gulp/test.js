0679899ead574e0f62f81aeeec7416726ea87391
David Asabina <david@supr.nu>
2015-10-07 17:03:08 +0200

    compile and build before test :wink:

--- gulp/test.js
+++ gulp/test.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var babel = require('babel/register');
 
-gulp.task('test', function() {
+gulp.task('test', ['compile', 'build'], function() {
   return gulp
     .src('test/**/*.js', {
       read: false,
