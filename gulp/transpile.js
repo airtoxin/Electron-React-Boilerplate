0e87437f94efe60e9d46781fbd536c9135de77d2
David Asabina <david@supr.nu>
2015-10-08 16:18:25 +0200

    transpile all component js files

--- gulp/transpile.js
+++ gulp/transpile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('transpile', function() {
-  return gulp.src(['./src/**/*.js', './src/component'], { base: './' })
+  return gulp.src(['./src/**/*.js', './src/component/**/*.js'], { base: './' })
     .pipe(babel())
     .pipe(gulp.dest('./compile'));
 });
