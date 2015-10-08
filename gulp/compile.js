d4428236156e39cf8b352a5550cbe0642011a798
David Asabina <david@supr.nu>
2015-10-08 16:20:58 +0200

    perform compile steps synchronously

--- gulp/compile.js
+++ gulp/compile.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
-gulp.task('compile', [
-  'transpile',
-	'through',
-	'browserify',
-	'less',
-	'install',
-]);
+gulp.task('compile', function() {
+  runSequence(
+    'transpile',
+    'through',
+    'browserify',
+    'less',
+    'install'
+  );
+});
